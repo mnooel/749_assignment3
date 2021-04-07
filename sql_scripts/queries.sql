9d433a3dd618fddd916b95e9634e00101c344c79
michaelnoel <noel.michael87@gmail.com>
2021-04-06 19:57:54 -0500

    finished query 5 and 6

--- sql_scripts/queries.sql
+++ sql_scripts/queries.sql
@@ -22,20 +22,49 @@ WHERE CATALOGNO IN (
     SELECT CATALOGNO
     FROM MAKES
     WHERE STAGENAME = 'Tom Hanks'
-    AND ROLE = 'director'
-    );
+      AND ROLE = 'director'
+);
 
 -- Find which DVDs directed by Tom Hanks are currently available in a branch in Milwaukee.
 -- Get the title of every such DVD and the street address of the branch that currently has it.
--- TODO QUERY 4
+-- QUERY 4
+SELECT DISTINCT TITLE,
+                STREET
+FROM DVDCOPYLOCATION
+WHERE CITY = 'Milwaukee'
+  AND CATALOGNO IN (SELECT CATALOGNO FROM MAKES WHERE STAGENAME = 'Tom Hanks' AND ROLE = 'director')
+ORDER BY TITLE;
 
 -- Find which DVDs directed by Tom Hanks are currently available with multiple (more than one) copies in a branch in Milwaukee.
--- Get the title and number of copies of every such DVD and the street address of the branch that currently has it.
--- TODO QUERY 5
+-- -- Get the title and number of copies of every such DVD and the street address of the branch that currently has it.
+-- QUERY 5
+SELECT CL.TITLE,
+       COUNT(CL.TITLE) AS N_COPIES,
+       CL.STREET
+FROM DVDCOPYLOCATION CL
+WHERE CL.CATALOGNO IN (SELECT CATALOGNO FROM MAKES WHERE STAGENAME = 'Tom Hanks' AND ROLE = 'director')
+  AND CL.CITY = 'Milwaukee'
+GROUP BY CL.TITLE, CL.STREET
+HAVING COUNT(CL.TITLE) > 1;
 
 -- Find which DVDs directed by Tom Hanks are currently NOT available in any branch in Milwaukee but are available somewhere else.
--- Get the title of every such DVD and the address of the branch that currently has it.
--- TODO QUERY 6
+-- Get the title of every such DVD and  the address of the branch that currently has it.
+-- QUERY 6
+SELECT DISTINCT TITLE, STREET, CITY, STATE, ZIP
+FROM DVDCOPYLOCATION
+WHERE CATALOGNO IN
+((
+    SELECT DISTINCT CATALOGNO
+    FROM DVDCOPYLOCATION
+    WHERE CATALOGNO IN (SELECT CATALOGNO FROM MAKES WHERE STAGENAME = 'Tom Hanks' AND ROLE = 'director')
+)
+MINUS
+(
+SELECT DISTINCT CATALOGNO
+FROM DVDCOPYLOCATION
+    WHERE CATALOGNO IN (SELECT CATALOGNO FROM MAKES WHERE STAGENAME = 'Tom Hanks' AND ROLE = 'director')
+    AND CITY = 'Milwaukee'
+));
 
 -- Find which DVDs directed by Tom Hanks are currently available in ALL branches in Milwaukee.
 -- Get the title of every such. (Hint: This query involves the Division operation in relational algebra, but is notoriously difficult to write in SQL.
@@ -44,26 +73,23 @@ WHERE CATALOGNO IN (
 
 -- List the first name, last name, and DOB of each staff member.
 -- QUERY 8
-SELECT
-       FIRSTNAME,
+SELECT FIRSTNAME,
        LASTNAME,
        DOB
 FROM STAFF;
 
 -- List the name and age of each staff member.
 -- QUERY 9
-SELECT
-       FIRSTNAME||' '||LASTNAME AS NAME,
-       FLOOR (MONTHS_BETWEEN(SYSDATE, DOB) / 12) AS AGE
+SELECT FIRSTNAME || ' ' || LASTNAME             AS NAME,
+       FLOOR(MONTHS_BETWEEN(SYSDATE, DOB) / 12) AS AGE
 FROM STAFF;
 
 -- List the name and age of each staff member.
 -- If he/her has a supervisor, also list the name of his/her supervisor.
 -- QUERY 10
-SELECT
-       S.FIRSTNAME||' '||S.LASTNAME AS NAME,
-       FLOOR (MONTHS_BETWEEN(SYSDATE, S.DOB) / 12) AS AGE,
+SELECT S.FIRSTNAME || ' ' || S.LASTNAME           AS NAME,
+       FLOOR(MONTHS_BETWEEN(SYSDATE, S.DOB) / 12) AS AGE,
        SUP.SUPERVISOR
-FROM
-     STAFF S LEFT OUTER JOIN (SELECT STAFFNO, FIRSTNAME||' '||LASTNAME AS SUPERVISOR FROM STAFF) SUP
-         ON SUP.STAFFNO = S.SUPERVISOR;
+FROM STAFF S
+         LEFT OUTER JOIN (SELECT STAFFNO, FIRSTNAME || ' ' || LASTNAME AS SUPERVISOR FROM STAFF) SUP
+                         ON SUP.STAFFNO = S.SUPERVISOR;
