ea225c4ba7e5e1b6ceac1e4d88e666797e66d6ec
michaelnoel <noel.michael87@gmail.com>
2021-04-06 19:57:38 -0500

    created view

--- tear_down_and_build.py
+++ tear_down_and_build.py
@@ -3,6 +3,7 @@
 from engine import OracleConnection
 from drop_tables import drop_tables
 from create_tables import create_tables
+from create_views import create_views
 from insert_data import insert_csv_data_into_oracle
 
 
@@ -10,5 +11,6 @@ if __name__ == '__main__':
     conn = OracleConnection()
     drop_tables(connection=conn)
     create_tables(connection=conn)
+    create_views(connection=conn)
     tables_list = ['Branch', 'Staff', 'ActorDirector', 'DVD', 'DVDCopy', 'Makes']
     insert_csv_data_into_oracle(connection=conn, tables=tables_list)
