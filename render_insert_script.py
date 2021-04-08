03b86be3e093cf542ecaec6f9c5ebdfd51aedc69
Michael Noel <mike@corecreative.com>
2021-04-08 11:51:09 -0500

    wrote script to render insert statements in .sql file

---
+++ render_insert_script.py
@@ -0,0 +1,39 @@
+# render_insert_script.py
+
+import pandas as pd
+from engine import Record
+
+
+def render_insert_script(file_name: str, tables: list):
+
+    file = open(f'sql_scripts/{file_name}', 'w')
+
+    file.write('-- BUS-ADM 749 Data and Information Management\n')
+    file.write('-- Michael Noel\n')
+    file.write('-- Continental Palms DVD\n')
+    file.write(f'-- {file_name}\n\n')
+
+    for table in tables:
+
+        file.write(f'-- Insert {table} data\n')
+
+        # data df
+        data_df = pd.read_csv(f'external_data/{table}.csv')
+        data_df = data_df.where(pd.notnull(data_df), None)
+
+        record_objects = []
+
+        for record in data_df.to_dict(orient='records'):
+            record_objects.append(Record(table_name=table, **record))
+
+        for record in record_objects:
+            file.write(record.render_insert_string() + ';\n')
+
+        file.write('\n')
+
+    file.close()
+
+
+if __name__ == '__main__':
+    tables = ['Branch', 'Staff', 'ActorDirector', 'DVD', 'DVDCopy', 'Makes']
+    render_insert_script(file_name='insert_data.sql',tables=tables)
---
+++ sql_scripts/insert_data.sql
@@ -0,0 +1,73 @@
+-- BUS-ADM 749 Data and Information Management
+-- Michael Noel
+-- Continental Palms DVD
+-- insert_data.sql
+
+-- Insert Branch data
+INSERT INTO Branch (branchNo, street, city, state, zip) VALUES ('WI01', '100 E Capital Dr', 'Milwaukee', 'WI', 53201);
+INSERT INTO Branch (branchNo, street, city, state, zip) VALUES ('WI02', '250 N Swan Ave', 'Milwaukee', 'WI', 53217);
+INSERT INTO Branch (branchNo, street, city, state, zip) VALUES ('WI03', '750 W Mequon Rd', 'Milwaukee', 'WI', 53221);
+INSERT INTO Branch (branchNo, street, city, state, zip) VALUES ('IL01', '844 E Milwaukee Ave', 'Chicago', 'IL', 60601);
+INSERT INTO Branch (branchNo, street, city, state, zip) VALUES ('IL02', '777 W Canal St', 'Chicago', 'IL', 60612);
+
+-- Insert Staff data
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0101', 'Bob', 'Johnstone', 'manager', 58000, '12-Dec-50', 'WI01', Null);
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0102', 'Dan', 'Nieley', 'supervisor', 48000, '17-Oct-60', 'WI01', 'WI0101');
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0103', 'Lisa', 'Swan', 'assistant', 38000, '1-Jan-90', 'WI01', 'WI0102');
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0104', 'John', 'Smith', 'assistant', 38000, '21-Feb-81', 'WI01', 'WI0102');
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0201', 'Lance', 'Oniel', 'manager', 68000, '12-Dec-68', 'WI02', Null);
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0202', 'Linda', 'Zahedi', 'supervisor', 58000, '17-Oct-87', 'WI02', 'WI0201');
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0203', 'Jennifer', 'Haseman', 'supervisor', 56000, '1-Jan-86', 'WI02', 'WI0201');
+INSERT INTO Staff (staffNo, firstName, lastName, position, salary, DOB, branchNo, supervisor) VALUES ('WI0204', 'Ben', 'Sinha', 'supervisor', 52000, '21-Feb-88', 'WI02', 'WI0201');
+
+-- Insert ActorDirector data
+INSERT INTO ActorDirector (stageName, name, gender) VALUES ('Tom Hanks', 'Thomas Jeffrey Hanks', 'M');
+INSERT INTO ActorDirector (stageName, name, gender) VALUES ('Robert Zemeckis', 'Robert Lee Zemeckis', 'M');
+INSERT INTO ActorDirector (stageName, name, gender) VALUES ('Liv Tyler', 'Liv Rundgren Tyler', 'F');
+INSERT INTO ActorDirector (stageName, name, gender) VALUES ('Steven Spielberg', 'Steven A Spielberg', 'M');
+INSERT INTO ActorDirector (stageName, name, gender) VALUES ('Bruce Lee', 'Bruce Lee', 'M');
+
+-- Insert DVD data
+INSERT INTO DVD (catalogNo, title, category, dailyRent) VALUES ('D00001', 'Forrest Gump', 'drama', 1.99);
+INSERT INTO DVD (catalogNo, title, category, dailyRent) VALUES ('D00002', 'That Thing You Do!', 'drama', 1.99);
+INSERT INTO DVD (catalogNo, title, category, dailyRent) VALUES ('D00003', 'Saving Private Ryan', 'drama', 1.99);
+INSERT INTO DVD (catalogNo, title, category, dailyRent) VALUES ('D00004', 'Band of Brothers', 'tv series', 9.99);
+INSERT INTO DVD (catalogNo, title, category, dailyRent) VALUES ('D00005', 'Larry Crowne', 'drama', 2.99);
+INSERT INTO DVD (catalogNo, title, category, dailyRent) VALUES ('D00006', 'From the Earth to the Moon', 'tv series', 10.99);
+INSERT INTO DVD (catalogNo, title, category, dailyRent) VALUES ('D00007', 'Way of the Dragon', 'action', 1.99);
+
+-- Insert DVDCopy data
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00001', 1, 2, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00001', 2, 2, 'WI02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00001', 3, 3, 'WI03');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00002', 1, 1, 'IL01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00002', 2, 1, 'IL01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00002', 3, 1, 'IL02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00003', 1, 1, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00004', 1, 1, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00004', 2, 1, 'WI02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00004', 3, 1, 'IL02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00005', 1, 1, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00005', 2, 1, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00005', 3, 1, 'WI02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00005', 4, 1, 'WI02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00006', 1, 1, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00006', 2, 1, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00006', 3, 2, 'WI02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00006', 4, 1, 'WI03');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00007', 1, 3, 'WI01');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00007', 2, 4, 'WI02');
+INSERT INTO DVDCopy (catalogNo, copyNo, condition, branchNo) VALUES ('D00007', 3, 4, 'WI03');
+
+-- Insert Makes data
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Tom Hanks', 'D00001', 'lead actor');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Robert Zemeckis', 'D00001', 'director');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Tom Hanks', 'D00002', 'director');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Liv Tyler', 'D00002', 'lead actress');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Tom Hanks', 'D00003', 'lead actor');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Steven Spielberg', 'D00003', 'director');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Tom Hanks', 'D00004', 'director');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Tom Hanks', 'D00005', 'director');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Tom Hanks', 'D00006', 'director');
+INSERT INTO Makes (stageName, catalogNo, role) VALUES ('Bruce Lee', 'D00007', 'director');
+
